c417769c99082bfdc2ed13baae9369e64e85e608
Amir-Mohammed <59926940+Amir-Mohammed@users.noreply.github.com>
2020-06-20 14:26:27 +0200

    add unique validation

--- routes/users.js
+++ routes/users.js
@@ -21,4 +21,38 @@ router.post("/login", async (req, res, next) => {
   throw new CustomError("Email not exists", 401);
 });
 
+router.post("/unique-email", async (req, res, next) => {
+  let email = req.body.email;
+  let student = await Student.findOne({ email });
+  if (student) {
+    return res.json({unique: false});
+  }
+  let instructor = await Instructor.findOne({ email });
+  if (instructor) {
+    return res.json({unique: false});
+  }
+  let community = await Community.findOne({ email });
+  if (community) {
+    return res.json({unique: false});
+  }
+  return res.json({unique: true});  
+})
+
+router.post("/unique-username", async (req, res, next) => {
+  let username = req.body.username;
+  let student = await Student.findOne({ username });
+  if (student) {
+    return res.json({unique: false});
+  }
+  let instructor = await Instructor.findOne({ username });
+  if (instructor) {
+    return res.json({unique: false});
+  }
+  let community = await Community.findOne({ username });
+  if (community) {
+    return res.json({unique: false});
+  }
+  return res.json({unique: true});  
+})
+
 module.exports = router;
